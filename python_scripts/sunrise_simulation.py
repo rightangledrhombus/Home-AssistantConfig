c29bf8f219520d3ec5a9a2260c1b8c762134877f
rightangledrhombus <km.rilling@gmail.com>
2017-09-17 23:52:38 +0000

    added living room lamp to morning simulation

--- python_scripts/sunrise_simulation.py
+++ python_scripts/sunrise_simulation.py
@@ -2,17 +2,18 @@
 
 # Set Params
 #brightness goes from 0 - 255
-brightness_increment = 15
-brightness_max = 175
+brightness_increment = 10
+brightness_max = 130
 brightness_min = 0
 # mired = 1,000,000/kelvin
 mired_increment = 25
 mired_max = 450
-mired_min = 250
+mired_min = 150
 
-# Get current brightness value
+# set lights to change
 entity_id1 = 'light.kitchen_left'
 entity_id2 = 'light.kitchen_right'
+entity_id3 = 'light.living_room_lamp_tv'
 
 # get attributes of light
 states = hass.states.get(entity_id1)
@@ -23,7 +24,7 @@ mired = states.attributes.get('color_temp')
 new_brightness = brightness + brightness_increment
 new_mired = mired - mired_increment
 
-# mired values for color temp go from approx. 450 (warm) to 170 (cold).
+# mired values for color temp go from approx.
 # mired = 1,000,000/kelvin
 if (new_mired < mired_min): new_mired = mired_min
 elif (new_mired > mired_max): new_mired = mired_max
@@ -35,4 +36,6 @@ elif (new_brightness > brightness_max): new_brightness = brightness_max
 data = { "entity_id" : entity_id1, "brightness" : new_brightness, "color_temp" : new_mired}
 hass.services.call('light', 'turn_on', data)
 data = { "entity_id" : entity_id2, "brightness" : new_brightness, "color_temp" : new_mired}
+hass.services.call('light', 'turn_on', data)
+data = { "entity_id" : entity_id3, "brightness" : new_brightness, "color_temp" : new_mired}
 hass.services.call('light', 'turn_on', data)
