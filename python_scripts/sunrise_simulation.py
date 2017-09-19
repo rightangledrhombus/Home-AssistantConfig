1b50c264b96bc913ed2fd0f5c52047c78a7b4704
rightangledrhombus <km.rilling@gmail.com>
2017-09-19 03:12:09 +0000

    changed sunrise lights and params; refactored sunrise script

--- python_scripts/sunrise_simulation.py
+++ python_scripts/sunrise_simulation.py
@@ -2,21 +2,20 @@
 
 # Set Params
 #brightness goes from 0 - 255
-brightness_increment = 10
 brightness_max = 130
 brightness_min = 0
+brightness_increment = 10
+
 # mired = 1,000,000/kelvin
-mired_increment = 25
 mired_max = 450
-mired_min = 150
+mired_min = 225
+mired_increment = 15
 
 # set lights to change
-entity_id1 = 'light.kitchen_left'
-entity_id2 = 'light.kitchen_right'
-entity_id3 = 'light.living_room_lamp_tv'
+entity_ids = ['light.kitchen_left', 'light.kitchen_right', 'light.living_room_lamp_tv', 'light.guest_bathroom']
 
 # get attributes of light
-states = hass.states.get(entity_id1)
+states = hass.states.get(entity_ids[0])
 brightness = states.attributes.get('brightness')
 mired = states.attributes.get('color_temp')
 
@@ -33,9 +32,6 @@ if (new_brightness < brightness_min): new_brightness = brightness_min
 elif (new_brightness > brightness_max): new_brightness = brightness_max
 
 # Call service
-data = { "entity_id" : entity_id1, "brightness" : new_brightness, "color_temp" : new_mired}
-hass.services.call('light', 'turn_on', data)
-data = { "entity_id" : entity_id2, "brightness" : new_brightness, "color_temp" : new_mired}
-hass.services.call('light', 'turn_on', data)
-data = { "entity_id" : entity_id3, "brightness" : new_brightness, "color_temp" : new_mired}
-hass.services.call('light', 'turn_on', data)
+for i in range(0,len(entity_ids)):
+    data = { "entity_id" : entity_ids[i], "brightness" : new_brightness, "color_temp" : new_mired}
+    hass.services.call('light', 'turn_on', data)
