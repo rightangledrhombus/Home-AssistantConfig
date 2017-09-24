1806d8b75f219272d8ec6ba00b59d7ee3f4238e2
rightangledrhombus <km.rilling@gmail.com>
2017-09-24 19:59:00 +0000

    added new lights; added new icon customization; fixed emulated hue duplicates; had sunrise simulation slightly better

--- python_scripts/sunrise_simulation.py
+++ python_scripts/sunrise_simulation.py
@@ -2,36 +2,45 @@
 
 # Set Params
 #brightness goes from 0 - 255
-brightness_max = 130
+brightness_max = 175
 brightness_min = 0
 brightness_increment = 10
 
 # mired = 1,000,000/kelvin
 mired_max = 450
 mired_min = 225
-mired_increment = 15
+mired_increment = 20
 
 # set lights to change
-entity_ids = ['light.kitchen_left', 'light.kitchen_right', 'light.living_room_lamp_tv', 'light.guest_bathroom']
+entity_ids = ['light.kitchen_left', 'light.kitchen_right', 'light.living_room_lamp_tv', 'light.gaming_room_lamp']
 
-# get attributes of light
-states = hass.states.get(entity_ids[0])
-brightness = states.attributes.get('brightness')
-mired = states.attributes.get('color_temp')
+# initialize all lists
+n_lights = len(entity_ids)
+states = [None] * n_lights
+brightness = [None] * n_lights
+mired = [None] * n_lights
+new_brightness = [None] * n_lights
+new_mired = [None] * n_lights
 
-# set new data
-new_brightness = brightness + brightness_increment
-new_mired = mired - mired_increment
+# loop through lists, find state values and set new ones
+for i in range(0,n_lights):
+    # get attributes of light
+    states[i] = hass.states.get(entity_ids[i])
+    brightness[i] = states[i].attributes.get('brightness')
+    mired[i] = states[i].attributes.get('color_temp')
 
-# mired values for color temp go from approx.
-# mired = 1,000,000/kelvin
-if (new_mired < mired_min): new_mired = mired_min
-elif (new_mired > mired_max): new_mired = mired_max
+    # set new data
+    new_brightness[i] = brightness[i] + brightness_increment
+    new_mired[i] = mired[i] - mired_increment
 
-if (new_brightness < brightness_min): new_brightness = brightness_min
-elif (new_brightness > brightness_max): new_brightness = brightness_max
+    # mired values for color temp go from approx.
+    # mired = 1,000,000/kelvin
+    if (new_mired[i] < mired_min): new_mired[i] = mired_min
+    elif (new_mired[i] > mired_max): new_mired[i] = mired_max
 
-# Call service
-for i in range(0,len(entity_ids)):
-    data = { "entity_id" : entity_ids[i], "brightness" : new_brightness, "color_temp" : new_mired}
+    if (new_brightness[i] < brightness_min): new_brightness[i] = brightness_min
+    elif (new_brightness[i] > brightness_max): new_brightness[i] = brightness_max
+
+    # Call service
+    data = { "entity_id" : entity_ids[i], "brightness" : new_brightness[i], "color_temp" : new_mired[i]}
     hass.services.call('light', 'turn_on', data)
